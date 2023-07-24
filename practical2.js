a3e44817ee9dc83deba214266db3f5a6ad9b7edf
sabatali <126473010+sabatali@users.noreply.github.com>
2023-07-24 13:49:30 +0500

    Create practical2.js

---
+++ practical2.js
@@ -0,0 +1,12 @@
+// write a program that display a character value and its ASCII Value
+
+// Prompt the user to enter a character value
+let inputCharacter = prompt("enter character : ");
+
+// Get the ASCII value of the entered character
+
+let asciiValue =inputCharacter.charCodeAt(0);
+
+// Display the character value and its ASCII value on the screen
+console.log("character is :"+ inputCharacter);
+console.log("character is :"+ asciiValue);
