dbd60034c076c1a209d0b19d169878447d57b01a
sabatali <126473010+sabatali@users.noreply.github.com>
2023-07-24 13:16:41 +0500

    Create practicals.js

---
+++ practicals.js
@@ -0,0 +1,9 @@
+// Declare and initialize two variables of int data type
+let num1 = 34;
+let num2 = 56;
+
+// Calculate the sum of the two variables
+let sum = num1 + num2;
+
+// Display the result on the screen
+console.log("The sum is: " + sum);
